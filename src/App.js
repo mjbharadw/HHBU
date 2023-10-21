9351e261353c8f0aa03718d15714c032e9d4383d
graceelias <91794990+graceelias@users.noreply.github.com>
2023-10-21 15:26:19 -0400

    working on variables

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [exercied, setExercised] = useState(false);
-  const [slept, setSlept] = useState(false);
-  const [reflected, setReflected] = useState(false);
-  const [read, setRead] = useState(false);
-  const [walked, setWalked] = useState(false);
-  const [cleaned, setCleaned] = useState(false);
+  const [exercised, setExercised] = useState(0);
+  const [slept, setSlept] = useState(0);
+  const [reflected, setReflected] = useState(0);
+  const [read, setRead] = useState(0);
+  const [walked, setWalked] = useState(0);
+  const [cleaned, setCleaned] = useState(0);
   const [waterDrank, setWaterDrank] = useState(0);
+
+  const handleExercisedClicked = () =>
+  {
+    setExercised(exercised + 1);
+  }
+
+  const handleSleptClicked = () =>
+  {
+    setSlept(slept + 1);
+  }
+
+  const handleReflectedClicked = () =>
+  {
+    setReflected(reflected + 1);
+  }
+
+  const handleReadClicked = () =>
+  {
+    setRead(read + 1);
+  }
+
+  const handleWalkedClicked = () =>
+  {
+    setWalked(walked + 1);
+  }
+
+  const handleCleanedClicked = () =>
+  {
+    setCleaned(cleaned + 1);
+  }
 
   const plusClicked = () =>
   {
@@ -30,30 +60,26 @@ function App() {
       <div className="subtitle"> 
         Today I...
       </div>
-      <div className = "checkbox_container">
-        <button type="button" className="plus_minus_button" onClick={minusClicked}> - </button>
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Exercised
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Slept 7-10 hours
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Reflected
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Read
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Went on a Walk
-        <input type="checkbox" text="Exercised" className="checkbox"></input>
-          Cleaned Something
-      </div>
-      <div className= "water">
-        {waterDrank == 1 ? "Drank 1 Cup of Water" : "Drank " + waterDrank + " Cups of Water" }
-        <div>
-          <button type="button" className="plus_minus_button" onClick={minusClicked}> - </button>
-          <button type="button" className="plus_minus_button" onClick={plusClicked}> + </button>
-        </div>
+      <div className = "button_container">
+        <button type="button" className="button" onClick={handleExercisedClicked}> Exercised </button>
+        <button type="button" className="button" onClick={handleSleptClicked}> Slept </button>
+        <button type="button" className="button" onClick={handleReflectedClicked}> Reflected </button>
+        <button type="button" className="button" onClick={handleReadClicked}> Read </button>
+        <button type="button" className="button" onClick={handleWalkedClicked}> Walked </button>
+        <button type="button" className="button" onClick={handleCleanedClicked}> Cleaned </button>
+          {waterDrank == 1 ? "Drank 1 Cup of Water" : "Drank " + waterDrank + " Cups of Water" }
+          <div>
+            <button type="button" className="plus_minus_button" onClick={minusClicked}> - </button>
+            <button type="button" className="plus_minus_button" onClick={plusClicked}> + </button>
+          </div>
       </div>
       <div>
-        <button type="button" className="submit_button"> Submit Today's Log </button>
+      {exercised}
+      {slept}
+      {reflected}
+      {read}
+      {walked}
+      {cleaned}
       </div>
     </div>
   );
